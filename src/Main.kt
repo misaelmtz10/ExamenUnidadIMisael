830796575f560a84d6eb22624c976729d130e99b
misaelmtz10 <85245368+misaelmtz10@users.noreply.github.com>
2022-05-18 17:25:06 -0500

    [UPDATE] Main

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,62 @@
-fun main(args : Array<String>){
+import java.util.*
 
+fun main (args: Array<String>){
+    val scanner = Scanner(System.`in`)
+    var opcMenPrincipal : Int = 0
+    var opcMenSecundario : Int = 0
+    var nombre : String?
+    var descripcion : String?
+
+    //val programingCourse = ProgramingCourse()
+
+    do {
+        println("Bienvenido al sistema de cursos")
+        println("1) Registro de curso de cocina \n 2) Registro de curso de programación \n 3) Obtener todos los cursos \n 4) Salir \n Elige...")
+        opcMenPrincipal = scanner.nextInt()
+
+        when(opcMenPrincipal){
+            1 -> {
+                do {
+                    println("Menú para el curso de Cocina \n 1) Registrar curso de cocina \n 2) Actualizar curso de cocina \n 3) " +
+                            "Eliminar curso de cocina \n 4) Obtener todos los cursos de cocina \n 5) Regresar al menú principal \n Elige...")
+                    opcMenSecundario = scanner.nextInt()
+
+                    when(opcMenSecundario){
+                        1 -> {
+
+                            break;
+                        }
+                        2 -> {
+                            break;
+                        }
+                        3 -> {
+                            break;
+                        }
+                        4 -> {
+                            break;
+                        }
+                        5 -> {
+                            println("Regresando al menú principal")
+                            break;
+                        }
+                        else -> println("Opcción invalida")
+                    }
+                }while (opcMenSecundario != 5)
+
+
+            }
+            2 -> {
+                println("1) Registrar curso de programación \n 2) Actualizar curso de programación \n 3) " +
+                        "Eliminar curso de programación \n 4) Obtener todos los cursos de programación \n 5) Regresar al menú principal \n Elige...")
+                opcMenSecundario = scanner.nextInt()
+            }
+            3 -> {
+                // aqui va la consulta general
+            }
+            4 -> {
+                println("Saliendo...")
+            }
+            else -> println("Opcción invalida")
+        }
+    }while (opcMenPrincipal != 4)
 }
