ff42ed92ef6b0ec9656b50e45a0e0955703c98f1
LuisAbner <70979091+LuisAbner@users.noreply.github.com>
2022-05-18 17:52:22 -0500

    Update CookingCourse.kt

--- src/CookingCourse.kt
+++ src/CookingCourse.kt
@@ -1,8 +1,22 @@
 open class CookingCourse( id : Int, name : String, description  : String) : CourseAttr (id,name,description) {
+
+}
+fun main(args : Array<String>) {
+    val funcionesCocina: FuncionesCocina=FuncionesCocina()
+    funcionesCocina.add("luis","abner")
+    funcionesCocina.showAll()
+
+    funcionesCocina.delete(1)
+
+}
+
+class  FuncionesCocina{
+
     val list : MutableList<CookingCourse> =mutableListOf()
-    fun add( name : String, description  : String) {
+    fun add( name : String, description  : String):Int{
         val cookingCourseclass=CookingCourse(list.size+1,name,description)
         list.add(cookingCourseclass)
+        return list.size+1
     }
     fun delete(id : Int){
         val index = id -1
@@ -14,10 +28,8 @@ open class CookingCourse( id : Int, name : String, description  : String) : Cour
     }
     fun showAll(){
         for (i in list){
-            println(i)
+            println(i.name)
         }
     }
-}
-fun main(args : Array<String>) {
 
 }
