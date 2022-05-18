03cb1b2653c6a693bc53a602d88e37ac9669dba1
keyla <20133ti169@utez.edu.mx>
2022-05-18 17:47:19 -0500

    Companion Object
    Message Error, Message Success.

--- src/CourseAttr.kt
+++ src/CourseAttr.kt
@@ -1,2 +1,28 @@
 open class CourseAttr(var id : Int, var name : String,var description  : String) {
+    companion object{
+        fun getMessageRegister(id: Int,name : String, description  : String): Unit {
+            return if(id != null && name !="" &&  description != ""){
+                println("Registraste Exitosamente el Curso!")
+            }else{
+                println("Error! No se Registro Correctamente")
+            }
+        }
+
+        fun getMessageDelete(id: Int): Unit {
+            return if(id == null){
+                println("Borrado Exitosamente!")
+            }else{
+                println("Error! No se pudo Borrar")
+            }
+        }
+        fun getMessageActualizar(id: Int): Unit {
+            return if(id != id){
+                println("Actualizado Exitosamente!")
+            }else{
+                println("Error! No se pudo Actualizar")
+            }
+        }
+
+    }
+
 }
